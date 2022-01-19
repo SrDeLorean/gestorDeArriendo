d9a51a285de340c21b783325ae1417a63588ce0c
SrDeLorean <xebaelvemgador@gmail.com>
2022-01-19 17:35:04 -0300

    auth login

--- resources/js/src/router/index.js
+++ resources/js/src/router/index.js
@@ -15,55 +15,50 @@ import others from './routes/others'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  scrollBehavior() {
-    return { x: 0, y: 0 }
-  },
-  routes: [
-    { path: '/', redirect: { name: 'dashboard-ecommerce' } },
-    ...apps,
-    ...dashboard,
-    ...pages,
-    ...chartsMaps,
-    ...formsTable,
-    ...uiElements,
-    ...others,
-    {
-      path: '*',
-      redirect: 'error-404',
+    mode: 'history',
+    base: process.env.BASE_URL,
+    scrollBehavior() {
+        return { x: 0, y: 0 }
     },
-  ],
+    routes: [
+        { path: '/', redirect: { name: 'dashboard-ecommerce' } },
+        ...apps,
+        ...dashboard,
+        ...pages,
+        ...chartsMaps,
+        ...formsTable,
+        ...uiElements,
+        ...others,
+        {
+            path: '*',
+            redirect: 'error-404',
+        },
+    ],
 })
 
-router.beforeEach((to, _, next) => {
-  const isLoggedIn = isUserLoggedIn()
 
-  if (!canNavigate(to)) {
-    // Redirect to login if not logged in
-    if (!isLoggedIn) return next({ name: 'auth-login' })
+router.beforeEach((to, from, next) => {
+    const publicPages = ['/login', '/register', '/home'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('user');
 
-    // If logged in => not authorized
-    return next({ name: 'misc-not-authorized' })
-  }
-
-  // Redirect if logged in
-  if (to.meta.redirectIfLoggedIn && isLoggedIn) {
-    const userData = getUserData()
-    next(getHomeRouteForLoggedInUser(userData ? userData.role : null))
-  }
-
-  return next()
-})
+    // trying to access a restricted page + not logged in
+    // redirect to login page
+    if (authRequired && !loggedIn) {
+        next('/login');
+    } else {
+        next();
+    }
+});
 
 // ? For splash screen
 // Remove afterEach hook if you are not using splash screen
 router.afterEach(() => {
-  // Remove initial loading
-  const appLoading = document.getElementById('loading-bg')
-  if (appLoading) {
-    appLoading.style.display = 'none'
-  }
+    // Remove initial loading
+    const appLoading = document.getElementById('loading-bg')
+    if (appLoading) {
+        appLoading.style.display = 'none'
+    }
 })
 
 export default router
--- resources/js/src/views/pages/authentication/Login.vue
+++ resources/js/src/views/pages/authentication/Login.vue
@@ -213,6 +213,7 @@
 /* eslint-disable global-require */
 import { ValidationProvider, ValidationObserver } from 'vee-validate'
 import VuexyLogo from '@core/layouts/components/Logo.vue'
+import User from '@/models/user';
 import {
   BRow,
   BCol,
@@ -272,6 +273,7 @@ export default {
       // validation rules
       required,
       email,
+      user: new User('', '')
     }
   },
   computed: {
@@ -289,43 +291,20 @@ export default {
   },
   methods: {
     login() {
-      this.$refs.loginForm.validate().then(success => {
-        if (success) {
-          useJwt
-            .login({
-              email: this.userEmail,
-              password: this.password,
-            })
-            .then(response => {
-              const { userData } = response.data
-              useJwt.setToken(response.data.accessToken)
-              useJwt.setRefreshToken(response.data.refreshToken)
-              localStorage.setItem('userData', JSON.stringify(userData))
-              this.$ability.update(userData.ability)
-
-              // ? This is just for demo purpose as well.
-              // ? Because we are showing eCommerce app's cart items count in navbar
-              this.$store.commit('app-ecommerce/UPDATE_CART_ITEMS_COUNT', userData.extras.eCommerceCartItemsCount)
-
-              // ? This is just for demo purpose. Don't think CASL is role based in this case, we used role in if condition just for ease
-              this.$router.replace(getHomeRouteForLoggedInUser(userData.role)).then(() => {
-                this.$toast({
-                  component: ToastificationContent,
-                  position: 'top-right',
-                  props: {
-                    title: `Welcome ${userData.fullName || userData.username}`,
-                    icon: 'CoffeeIcon',
-                    variant: 'success',
-                    text: `You have successfully logged in as ${userData.role}. Now you can start to explore!`,
-                  },
-                })
-              })
-            })
-            .catch(error => {
-              this.$refs.loginForm.setErrors(error.response.data.error)
-            })
-        }
-      })
+      this.user.email = this.userEmail
+      this.user.password = this.password
+      this.$store.dispatch('auth/login', this.user).then(
+            () => {
+              this.$router.push('/dashboard/ecommerce');
+            },
+            error => {
+              this.loading = false;
+              this.message =
+                (error.response && error.response.data) ||
+                error.message ||
+                error.toString();
+            }
+          );
     },
   },
 }
